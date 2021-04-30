7269913109987c6e78bfc9ba386d0c53b149bf2d
neg2led <4232981+neg2led@users.noreply.github.com>
2021-04-30 21:56:53 +1000

    compiles!

--- README.md
+++ README.md
@@ -1,32 +1,5 @@
-# _Sample project_
+# bme68x-idf-example
 
-(See the README.md file in the upper level 'examples' directory for more information about examples.)
+example of how to use my bme68x-idf component, using a hacked up version of the i2cdev component from [UncleRus/esp-idf-lib](https://github.com/UncleRus/esp-idf-lib)
 
-This is the simplest buildable example. The example is used by command `idf.py create-project`
-that copies the project to user specified path and set it's name. For more information follow the [docs page](https://docs.espressif.com/projects/esp-idf/en/latest/api-guides/build-system.html#start-a-new-project)
-
-
-
-## How to use example
-We encourage the users to use the example as a template for the new projects.
-A recommended way is to follow the instructions on a [docs page](https://docs.espressif.com/projects/esp-idf/en/latest/api-guides/build-system.html#start-a-new-project).
-
-## Example folder contents
-
-The project **sample_project** contains one source file in C language [main.c](main/main.c). The file is located in folder [main](main).
-
-ESP-IDF projects are built using CMake. The project build configuration is contained in `CMakeLists.txt`
-files that provide set of directives and instructions describing the project's source files and targets
-(executable, library, or both). 
-
-Below is short explanation of remaining files in the project folder.
-
-```
-├── CMakeLists.txt
-├── main
-│   ├── CMakeLists.txt
-│   └── main.c
-└── README.md                  This is the file you are currently reading
-```
-Additionally, the sample project contains Makefile and component.mk files, used for the legacy Make based build system. 
-They are not used or needed when building with CMake and idf.py.
+this has been tested to compile on an esp32-c3 but it has not been tested to, you know, *actually work*
---
+++ main/src/forced_mode.c
@@ -0,0 +1,100 @@
+/**
+ * Copyright (C) 2021 Bosch Sensortec GmbH
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ *
+ */
+
+#include <stdio.h>
+
+#include <freertos/FreeRTOS.h>
+#include <freertos/task.h>
+
+#include <esp_err.h>
+#include <esp_log.h>
+#include <esp_timer.h>
+
+#include "bme68x-idf.h"
+#include "bme68x.h"
+
+/***********************************************************************/
+/*                         Macros                                      */
+/***********************************************************************/
+
+/* Macro for count of samples to be displayed */
+#define SAMPLE_COUNT UINT16_C(300)
+
+/***********************************************************************/
+/*                         Test code                                   */
+/***********************************************************************/
+static const char *TAG = "bme68x";
+
+void app_main(void) {
+    struct bme68x_dev bme;
+    int8_t rslt;
+    struct bme68x_conf conf;
+    struct bme68x_heatr_conf heatr_conf;
+    struct bme68x_data data;
+    uint32_t del_period;
+    uint32_t time_ms = 0;
+    uint8_t n_fields;
+    uint16_t sample_count = 1;
+
+    /* Interface preference is updated as a parameter
+     * For I2C : BME68X_I2C_INTF
+     * For SPI : BME68X_SPI_INTF
+     */
+    rslt = bme68x_interface_init(&bme, BME68X_I2C_INTF);
+    bme68x_check_rslt("bme68x_interface_init", rslt);
+
+    rslt = bme68x_init(&bme);
+    bme68x_check_rslt("bme68x_init", rslt);
+
+    /* Check if rslt == BME68X_OK, report or handle if otherwise */
+    conf.filter  = BME68X_FILTER_OFF;
+    conf.odr     = BME68X_ODR_NONE;
+    conf.os_hum  = BME68X_OS_16X;
+    conf.os_pres = BME68X_OS_1X;
+    conf.os_temp = BME68X_OS_2X;
+    rslt         = bme68x_set_conf(&conf, &bme);
+    bme68x_check_rslt("bme68x_set_conf", rslt);
+
+    /* Check if rslt == BME68X_OK, report or handle if otherwise */
+    heatr_conf.enable     = BME68X_ENABLE;
+    heatr_conf.heatr_temp = 300;
+    heatr_conf.heatr_dur  = 100;
+
+    rslt = bme68x_set_heatr_conf(BME68X_FORCED_MODE, &heatr_conf, &bme);
+    bme68x_check_rslt("bme68x_set_heatr_conf", rslt);
+
+    printf("Sample, TimeStamp(ms), Temperature(deg C), Pressure(Pa), Humidity(%%), Gas resistance(ohm), Status\n");
+
+    while (sample_count <= SAMPLE_COUNT) {
+        rslt = bme68x_set_op_mode(BME68X_FORCED_MODE, &bme);
+        bme68x_check_rslt("bme68x_set_op_mode", rslt);
+
+        /* Calculate delay period in microseconds */
+        del_period = bme68x_get_meas_dur(BME68X_FORCED_MODE, &conf, &bme) + (heatr_conf.heatr_dur * 1000);
+        bme.delay_us(del_period, bme.intf_ptr);
+
+        time_ms = (esp_timer_get_time() / 1000);
+
+        /* Check if rslt == BME68X_OK, report or handle if otherwise */
+        rslt = bme68x_get_data(BME68X_FORCED_MODE, &data, &n_fields, &bme);
+        bme68x_check_rslt("bme68x_get_data", rslt);
+
+        if (n_fields) {
+#ifdef BME68X_USE_FPU
+            ESP_LOGI(TAG, "%u, %lu, %.2f, %.2f, %.2f, %.2f, 0x%x\n", sample_count, (long unsigned int)time_ms, data.temperature,
+                     data.pressure, data.humidity, data.gas_resistance, data.status);
+#else
+            ESP_LOGI(TAG, "%u, %lu, %d, %lu, %lu, %lu, 0x%x\n", sample_count, (long unsigned int)time_ms,
+                     (data.temperature / 100), (long unsigned int)data.pressure, (long unsigned int)(data.humidity / 1000),
+                     (long unsigned int)data.gas_resistance, data.status);
+#endif
+            sample_count++;
+        }
+    }
+
+    bme68x_interface_deinit(&bme);
+}
